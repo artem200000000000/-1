2cab46ed17104571faa0ad4808edc9cc93617c6c
artem200000000000 <144527944+artem200000000000@users.noreply.github.com>
2024-03-16 09:44:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from model import get_class
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command() 
+async def check(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.messege.attachments:    
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f'images/{file_name}')
+            result = get_class(model="keras_model.h5", labels="labels.txt", image=f'images/{file_name}')
+            await ctx.send(result)
+    else:
+        await ctx.send("вы выбрали загрузить картинку")
+
+bot.run("MTIxNTMyNTM2NzgyMTEzOTk4OA.GwyRxs.aK76UDvr1eLARHljn_8AQrZ4O2SEx1aIdEv9sA")
